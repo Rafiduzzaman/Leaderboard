c67625afcf6e1c3db59374128432112395115ba0
codeStackr <rdzaman20000@gmail.com>
2023-08-11 04:20:06 +0600

    Add basic layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./styles.css";
 
- function component() {
-   const element = document.createElement('div');
+const refreshButton = document.getElementById("refreshButton");
+const addButton = document.getElementById("addButton");
+const scoreboard = document.getElementById("scoreboard");
+const nameInput = document.getElementById("nameInput");
+const scoreInput = document.getElementById("scoreInput");
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// Load scores from local storage or initialize if not found
+let scores = JSON.parse(localStorage.getItem("scores")) || [
+  { name: 'Alice', score: 100 },
+  { name: 'Bob', score: 85 },
+  { name: 'Carol', score: 120 }
+];
 
-   return element;
- }
+refreshButton.addEventListener("click", refreshScoreboard);
+addButton.addEventListener("click", addScore);
 
- document.body.appendChild(component());
+function refreshScoreboard() {
+  scoreboard.innerHTML = "";
+  scores.forEach((entry, index) => {
+    const entryDiv = document.createElement("div");
+    entryDiv.innerHTML = `${index + 1}. ${entry.name}: ${entry.score}`;
+    scoreboard.appendChild(entryDiv);
+  });
+}
+
+function addScore() {
+  const name = nameInput.value;
+  const score = parseInt(scoreInput.value);
+  if (name && !isNaN(score)) {
+    scores.push({ name, score });
+
+    // Save scores to local storage
+    localStorage.setItem("scores", JSON.stringify(scores));
+
+    nameInput.value = "";
+    scoreInput.value = "";
+    refreshScoreboard();
+  }
+}
+
+// Initial call to populate the scoreboard on page load
+refreshScoreboard();
